d52fe5ffa40e4865232b11ca51848c3d0e4ff272
Adrien-Ray <a.raymond@codeur.online>
2023-01-02 16:21:35 +0100

    brainfuck with stupid

--- page-10.php
+++ page-10.php
@@ -1,6 +1,6 @@
 <?php
 // pro
-$GLOBALS["citationOnThumb"] = 'citation en attente';
+$GLOBALS["citationOnThumb"] = '';
 $GLOBALS["flexdirection"] = '';
 get_template_part( 'template-parts/page_thumb_exeption' );
 ?>
--- page-16.php
+++ page-16.php
@@ -1,6 +1,6 @@
 <?php
 // perso
-$GLOBALS["citationOnThumb"] = 'citation en attente';
+$GLOBALS["citationOnThumb"] = '';
 $GLOBALS["flexdirection"] = 'landingContent--noreverse';
 get_template_part( 'template-parts/page_thumb_exeption' );
 ?>
