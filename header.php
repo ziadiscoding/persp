241dc24884d449a3fc282aeb1620eeb0372f34e7
Adrien-Ray <a.raymond@codeur.online>
2023-01-13 10:18:20 +0100

    add meta desc

--- header.php
+++ header.php
@@ -14,6 +14,7 @@ if (!isset($this_name)||empty($this_name)) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $this_name; ?> | <?= get_bloginfo( 'name' ); ?></title>
+    <meta name="description" content="Marie-Aline GAVAND - La fabrique de perspectives - Cabinet de thérapie et de coaching. Psychothérapie / Thérapie brève / Soutien psychologique / Coaching de projets d'orientation et professionnels / Bilan de compétences / Analyse des pratiques professionnelles.  &#171;Psychopraticienne et coach professionnelle certifiée, j’accompagne jeunes et adultes, selon leurs besoins, à « se fabriquer des perspectives » personnelles et professionnelles&#187;." />
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
