2064e770b78206ebf478097a7bd1a090b3a1f931
lafabriqiu <lafabriqiu@ssh01.cluster023.gra.hosting.ovh.net>
2023-03-09 09:51:33 +0000

    header

--- header.php
+++ header.php
@@ -15,6 +15,17 @@ if (!isset($this_name)||empty($this_name)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $this_name; ?> | <?= get_bloginfo( 'name' ); ?></title>
     <meta name="description" content="Marie-Aline GAVAND - La fabrique de perspectives - Cabinet de thérapie et de coaching. Psychothérapie / Thérapie brève / Soutien psychologique / Coaching de projets d'orientation et professionnels / Bilan de compétences / Analyse des pratiques professionnelles.  &#171;Psychopraticienne et coach professionnelle certifiée, j’accompagne jeunes et adultes, selon leurs besoins, à « se fabriquer des perspectives » personnelles et professionnelles&#187;." />
+    
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GM48LBFTK7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GM48LBFTK7');
+</script>
+
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
